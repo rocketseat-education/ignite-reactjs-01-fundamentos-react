7dba58d88d62a73dae459b166d4c5bf5dbbe2a5b
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-05-10 20:30:15 -0300

    Estrutura da aplicação - Componente: Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.css';
 
+import igniteLogo from '../assets/ignite-logo.svg';
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
     </header>
   );
 }
