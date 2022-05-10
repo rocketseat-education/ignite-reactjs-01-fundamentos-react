278206a701bd93935044617b18fce390ce4d18ad
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-05-10 20:50:18 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { Header } from './components/Header';
 import { Post } from './Post'
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author="Diego Fernandes"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
-      />
-      <Post
-        author="Gabriel Buzzi"
-        content="Um novo post muito legal"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+          <Post
+            author="Gabriel Buzzi"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <strong>Diego Fernandes</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
