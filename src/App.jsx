134359540d5045db58e600c033f1aa234ab411e2
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-05-10 18:05:25 -0300

    Estrutura da aplicação - CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from './components/Header';
 import { Post } from './Post'
+
+import './styles.css';
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post
         author="Diego Fernandes"
         content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <strong>Ignite Feed</strong>
+    </header>
+  );
+}
