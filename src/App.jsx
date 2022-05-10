4f9f36d8f051b3a83facee2546bd8a4204d35e43
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-05-10 17:48:10 -0300

    Iniciando com React - Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Post } from './Post'
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Diego Fernandes"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+      />
+      <Post
+        author="Gabriel Buzzi"
+        content="Um novo post muito legal"
+      />
     </div>
   )
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,3 +1,8 @@
-export function Post() {
-  return <p>Post</p>
+export function Post(props) {
+  return (
+    <div>
+      <strong>{props.author}</strong>
+      <p>{props.content}</p>
+    </div>
+  )
 }
