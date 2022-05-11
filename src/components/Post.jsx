f1d562f3e47456b41bcc77b755c406dd2a8d9a50
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-05-11 15:05:39 -0300

    Estrutura da aplicação - Estilizações do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -18,8 +18,12 @@ export function Post() {
       <div className={styles.content}>
         <p>Fala galeraa 👋</p>
         <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p>👉<a href="">jane.design/doctorcare</a></p>
-        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+        <p><a href="">jane.design/doctorcare</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
       </div>
     </article>
   )
