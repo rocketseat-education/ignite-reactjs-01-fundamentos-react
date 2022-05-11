1d06f10558f32ecb6d27031808afa56c82a39311
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-05-11 15:25:38 -0300

    Estrutura da aplicação - Formulário de comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -25,6 +25,18 @@ export function Post() {
           <a href="">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
