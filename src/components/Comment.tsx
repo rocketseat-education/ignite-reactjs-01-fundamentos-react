5edd25e735df6c6bf8bfe709c0de00286993147e
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-05-12 20:24:15 -0300

    Aprimorando a aplicação - Extensão de interfaces

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -24,7 +24,11 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
 
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/diego3g.png"
+        alt=""
+      />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
