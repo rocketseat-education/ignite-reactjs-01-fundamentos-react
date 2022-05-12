94d20475a8a5ac53206b8be08ebb0575beea4965
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-05-12 17:48:00 -0300

    Os motores do React - Comunicação entre componentes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,11 @@ import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
@@ -16,7 +20,7 @@ export function Comment({ content }) {
               <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -34,6 +34,10 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(comment) {
+    console.log(`Deletar comentário ${comment}`)
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -77,7 +81,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment key={comment} content={comment} />
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          )
         })}
       </div>
     </article>
